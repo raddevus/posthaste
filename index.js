2fd4a0c785c947aa09b8cbb84a3d7cdc35aac8cd
raddevus <1radeutsch1@gmail.com>
2025-10-22 15:56:54 -0400

    missed .js and html files -- bad mistake.

---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on('window-all-closed', () => {
+  // Quit on all platforms, including macOS
+  app.quit();
+});
+/* app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});*/
